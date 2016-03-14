b210c91be45bb3a30a427e34a61c544f38d09daa
zc07accp <zf@zfdemacbook-pro.local>
2016-03-14 10:10:17 +0800

    Initial Commit

---
+++ PhoneLoan_Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhoneLoan_Demo
+//
+//  Created by zf on 16/3/14.
+//  Copyright © 2016年 PY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
